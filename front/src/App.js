6ef0f38b73b465e26f50e27c53ad149598bbbab7
João Gabriel Mendes <joaogabrielmendes047@gmail.com>
2025-05-09 17:28:10 -0300

    Base do frontend e layout feita com css e jsx

--- front/src/App.js
+++ front/src/App.js
@@ -1,42 +1,30 @@
-import { useState } from 'react';
-import './App.css';
+import React, {useState} from "react";
+import "./App.css";
 
-function App() {
-  const [texto, setTexto] = useState('');
-  const [resposta, setResposta] = useState(null);
+function App(){
+  const [clausula, setClausula] = useState("");
+  const [resposta, setResposta] = useState("");
 
-  const enviar = async () => {
-    const res = await fetch('http://localhost:8080/api/analisar-contrato', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ texto }),
-    });
+  return(
+    <div className="container">
+      <h1>JurisIA</h1>
 
-    const data = await res.json();
-    setResposta(data);
-  };
 
-  return (
-    <div>
-      <h1>Analisar Contrato</h1>
       <textarea
-        rows="10"
-        cols="50"
-        value={texto}
-        onChange={(e) => setTexto(e.target.value)}
-        placeholder="Cole o contrato aqui..."
+      className="inputSubmit"
+      placeholder="Insira um texto para análise"
+      value={clausula}
+      onChange={(e) => setClausula(e.target.value)}
+      rows={8}
       />
-      <br />
-      <button onClick={enviar}>Enviar</button>
 
-      {resposta && (
-        <div>
-          <h2>Resultado:</h2>
-          <pre>{JSON.stringify(resposta, null, 2)}</pre>
-        </div>
-      )}
+      <button type="button" onClick={'função para o submit'}>Enviar</button>
+
+      <div className="resultado">
+
+        <p>{resposta || "Nenhuma resposta ainda."}</p>
+      </div>
     </div>
   );
 }
-
 export default App;
