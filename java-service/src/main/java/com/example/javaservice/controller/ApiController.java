5d7ec54a9fa54c2d42c728c382da4335d639ab19
FernandoZMaciel <fernandozimmermannmaciel@gmail.com>
2025-05-09 21:45:38 -0300

    Adicionado end point para analisar pdf

--- java-service/src/main/java/com/example/javaservice/controller/ApiController.java
+++ java-service/src/main/java/com/example/javaservice/controller/ApiController.java
@@ -13,7 +13,7 @@ import java.io.IOException;
 import java.util.List;
 
 @RestController
-public class TesteController {
+public class ApiController {
 
 	@Autowired
 	private QnAnswerService qnAnswer;
